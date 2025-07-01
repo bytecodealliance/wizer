0611b6b96b4300da2ce8a270104b4c76d39fe80f
Pat Hickey <p.hickey@f5.com>
2025-07-01 07:29:59 -0700

    rename root `component-init` crate to `component-init-transform` (#10)
    * rename root `component-init` crate to `component-init-transform`
    
    and move it to a child directory.
    
    This leave the `component-init` crate name available for a guest crate
    that creates a nice DX for this feature.
    
    * nightly fmt
    
    * cargo doc needs wasm32-wasip2 installed because test-programs-artifacts build.rs uses it

--- wasmtime/src/lib.rs
+++ wasmtime/src/lib.rs
@@ -1,12 +1,12 @@
 use anyhow::{Context, Result, anyhow};
-use component_init::Invoker;
+use component_init_transform::Invoker;
 use wasmtime::{
     Config, Engine, Store,
     component::{Component, ComponentNamedList, Instance, Lift, Linker},
 };
 
 pub async fn initialize(component: &[u8]) -> Result<Vec<u8>> {
-    component_init::initialize(component, |instrumented| {
+    component_init_transform::initialize(component, |instrumented| {
         Box::pin(async move {
             let i = invoker(instrumented)
                 .await
