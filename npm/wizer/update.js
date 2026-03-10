e4b46a870c715603cb0fd1b89d2109036c0aa9ec
Victor Adossi <vadossi@cosmonic.com>
2026-03-10 21:43:36 +0900

    fix: update npm build script, replace plzmasdk with @napi-rs/lzma

--- npm/wizer/update.js
+++ npm/wizer/update.js
@@ -2,11 +2,14 @@
 
 import { fileURLToPath } from 'node:url';
 import { dirname, join, parse } from 'node:path';
-import { mkdir, writeFile, readFile } from "node:fs/promises";
+import { mkdir, writeFile, readFile, rm } from "node:fs/promises";
+import { Buffer } from 'node:buffer';
+
 import decompress from 'decompress';
 import decompressUnzip from 'decompress-unzip';
 import decompressTar from 'decompress-tar';
-import plzma from 'plzmasdk';
+import xz from '@napi-rs/lzma/xz';
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const tag = process.argv.slice(2).at(0).trim() || 'dev';
 const version = tag.startsWith('v') ? tag.slice(1) : tag;
@@ -62,69 +65,66 @@ let packages = {
         os: 'win32',
         cpu: 'x64',
     },
+};
+
+let response = await fetch(`https://api.github.com/repos/bytecodealliance/wizer/releases/tags/${tag}`);
+if (!response.ok) {
+    console.error(`Response from https://api.github.com/repos/bytecodealliance/wizer/releases/tags/${tag} was not ok`, response);
+    console.error(await response.text());
+    process.exit(1);
 }
 
-let response = await fetch(`https://api.github.com/repos/bytecodealliance/wizer/releases/tags/${tag}`)
-if (!response.ok) {
-    console.error(`Response from https://api.github.com/repos/bytecodealliance/wizer/releases/tags/${tag} was not ok`, response)
-    console.error(await response.text())
-    process.exit(1)
-}
-response = await response.json()
-const id = response.id
-let assets = await fetch(`https://api.github.com/repos/bytecodealliance/wizer/releases/${id}/assets`)
+response = await response.json();
+const id = response.id;
+let assets = await fetch(`https://api.github.com/repos/bytecodealliance/wizer/releases/${id}/assets`);
 if (!assets.ok) {
-    console.error(`Response from https://api.github.com/repos/bytecodealliance/wizer/releases/${id}/assets was not ok`, assets)
-    console.error(await response.text())
-    process.exit(1)
+    console.error(`Response from https://api.github.com/repos/bytecodealliance/wizer/releases/${id}/assets was not ok`, assets);
+    console.error(await response.text());
+    process.exit(1);
 }
-assets = await assets.json()
+assets = await assets.json();
 
 for (const [packageName, info] of Object.entries(packages)) {
-    const asset = assets.find(asset => asset.name === info.releaseAsset)
+    const asset = assets.find(asset => asset.name === info.releaseAsset);
     if (!asset) {
-        console.error(`Can't find an asset named ${info.releaseAsset} for the release https://github.com/bytecodealliance/wizer/releases/tag/${tag}`)
-        process.exit(1)
+        console.error(`Can't find an asset named ${info.releaseAsset} for the release https://github.com/bytecodealliance/wizer/releases/tag/${tag}`);
+        process.exit(1);
     }
-    const packageDirectory = join(__dirname, '../', packageName.split('/').pop())
-    await mkdir(packageDirectory, { recursive: true })
-    await writeFile(join(packageDirectory, 'package.json'), packageJson(packageName, tag, info.description, info.os, info.cpu))
-    await writeFile(join(packageDirectory, 'index.js'), indexJs(info.binaryAsset))
-    const browser_download_url = asset.browser_download_url;
-    const archive = await fetch(browser_download_url)
-    if (!archive.ok) {
-        console.error(`Response from ${browser_download_url} was not ok`, archive)
-        console.error(await response.text())
-        process.exit(1)
+
+    const packageDirectory = join(__dirname, '../', packageName.split('/').pop());
+    await mkdir(packageDirectory, { recursive: true });
+    await writeFile(join(packageDirectory, 'package.json'), packageJson(packageName, tag, info.description, info.os, info.cpu));
+    await writeFile(join(packageDirectory, 'index.js'), indexJs(info.binaryAsset));
+
+    const browser_download_url = asset.browser_download_url;;
+    const downloadResp = await fetch(browser_download_url);
+    if (!downloadResp.ok) {
+        console.error(`Response from ${browser_download_url} was not ok`, archive);
+        console.error(await response.text());
+        process.exit(1);
     }
-    let buf = await archive.arrayBuffer()
+    let downloadBytes = await downloadResp.arrayBuffer();
 
     // Need to decompress into the original tarball format for later use in the `decompress` function
     if (info.releaseAsset.endsWith('.xz')) {
-        const archiveDataInStream = new plzma.InStream(buf);
-        const decoder = new plzma.Decoder(archiveDataInStream, plzma.FileType.xz);
-        decoder.open();
-
-        // We know the xz archive only contains 1 file, the tarball
-        // We extract the tarball in-memory, for later use in the `decompress` function
-        const selectedItemsToStreams = new Map();
-        selectedItemsToStreams.set(decoder.itemAt(0), plzma.OutStream());
-
-        decoder.extract(selectedItemsToStreams);
-        for (const value of selectedItemsToStreams.values()) {
-            buf = value.copyContent()
-        }
+        // Decompress and write to local file on disk, then clean up the ephemeral file
+        downloadBytes = await xz.decompress(new Uint8Array(downloadBytes));
     }
-    await decompress(Buffer.from(buf), packageDirectory, {
-        // Remove the leading directory from the extracted file.
-        strip: 1,
-        plugins: [
-            decompressUnzip(),
-            decompressTar()
-        ],
-        // Only extract the binary file and nothing else
-        filter: file => parse(file.path).base === info.binaryAsset
-    })
+
+    await decompress(
+        Buffer.from(downloadBytes), 
+        packageDirectory, 
+        {
+            // Remove the leading directory from the extracted file.
+            strip: 1,
+            plugins: [
+                decompressUnzip(),
+                decompressTar()
+            ],
+            // Only extract the binary file and nothing else
+            filter: file => parse(file.path).base === info.binaryAsset
+        },
+    );
 }
 
 function indexJs(binaryAsset) {
